e12dec13ef2f44054ea291ea305db1194b595da8
Phil Kerrigan <phil.kerrigan@gmail.com>
2020-07-12 14:54:50 -0400

    gulp

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php global $active;
+    $active ='index';
+    include('header.php');
+?>
+
+hi tetsttslkdfhl
+  <!-- Add your site or application content here -->
+
+
+
+
+
+
+<?php include('footer.php')?>
