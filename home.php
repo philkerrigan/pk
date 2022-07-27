ef06e23735cbf0d13aad46de9ab1f57e7e1b3289
Phil Kerrigan <phil.kerrigan@gmail.com>
2022-07-27 19:36:42 -0400

    home

--- home.php
+++ home.php
@@ -1,6 +1,9 @@
-<?php /* Template Name: home */ ?>
 
-<?php get_header(); ?>
+
+<?php global $active;
+    $active ='home';
+    include('header.php');
+?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
@@ -178,23 +181,13 @@
 				</div>
 			</section>
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php
 
-get_footer();
+
+<?php include('footer.php')?>
+
+
