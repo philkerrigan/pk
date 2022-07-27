850976eb64706c2fd40b19c28d4af7e0d2d5ba30
Phil Kerrigan <phil.kerrigan@gmail.com>
2022-07-27 18:53:25 -0400

    home

---
+++ home.php
@@ -0,0 +1,200 @@
+<?php /* Template Name: home */ ?>
+
+<?php get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+			<section class="sample-1">
+				<div class="container-fluid">
+					<div class="sample-grid">
+
+						<div>
+							Farts Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</div>
+						<div>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</div>
+						<div>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</div>
+						<div>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</div>
+
+						<div class="nested">
+							<div>
+								Lorem ipsum dolor sit.
+							</div>
+							<div>
+								Lorem ipsum dolor sit.
+							</div>
+							<div>
+								Lorem ipsum dolor sit.
+							</div>
+						</div>
+						<div>
+							Lorem ipsum dolor sit.
+						</div>
+						<div>
+							Lorem ipsum dolor sit.
+						</div>
+						<div>
+							Lorem ipsum dolor sit.
+						</div>
+						<div>
+							Lorem ipsum dolor sit.
+						</div>
+						<div>
+							Lorem ipsum dolor sit.
+						</div>
+						<div>
+							Lorem ipsum dolor sit.
+						</div>
+						<div>
+							Lorem ipsum dolor sit.
+						</div>
+						<div>
+							Lorem ipsum dolor sit.
+						</div>
+						<div>
+							Lorem ipsum dolor sit.
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section class="sample-2">
+				<div class="container-fluid">
+					<div class="sample-grid">
+						<div class="box box1">Box 1</div>
+						<div class="box box2">Box 2</div>
+						<div class="box box3">Box 3</div>
+						<div class="box box4">Box 4</div>
+					</div>
+				</div>
+			</section>
+
+			Techincal
+			Adobe Creative Suite, HTML CSS/SCSS, Balsamiq, WordPress
+			(knowledge of Javascript, jQuery, Git, GitHub, Gulp)
+
+			<section class="resume">
+				<div class="wrap">
+					<div class="grid-experience">
+						<div>
+							Experience
+						</div>
+						<div class="experience-nested">
+
+							<div>
+								Adaptive Health (formely Direct Digital LLC), Sr. UI / UX Designer 02.2013–present
+							</div>
+							<div>
+								<ul>
+									<li>Provide logo, packaging, and web site design for new products</li>
+									<li>Develop UI / UX for internal CRM and shopping cart admin tool</li>
+									<li>Front-end development for all web related tasks</li>
+									<li>Spearheaded redesign and front end development of Adaptive Health's corperate website</li>
+								</ul>
+							</div>
+
+							<div>
+								Bigaxiom Creative, Co-founder and Creative Lead 10.2010–08.2017
+							</div>
+							<div>
+								<ul>
+									<li>Implement and maintain brand identity, web site design, and company brand positioning</li>
+									<li>Lead creative efforts from concept to production on cohesive brand materials for clients</li>
+									<li>Lead concept presentation of strategy and corresponding execution for web and print projects while driving the approval process to stay on deadline</li>
+									<li>Design user experience flows for client consensus before sending detailed wireframes to development team for precise and effective implementation</li>
+								</ul>
+							</div>
+
+							<div>
+								City of Boston, Web / Graphic Designer 01.2008-02.2013
+							</div>
+							<div>
+								<ul>
+									<li>Oversaw all graphic design services to support the Office of Business Development</li>
+									<li>Provided guidance, technical assistance, and designed consistent brand materials including logos, signage, and web sites to neighborhood districts, individual businesses and non-profits</li>
+									<li>Coordinated, reviewed, and executed cost effective production of marketing materials</li>
+									<li>Project managed contracted design agencies</li>
+									<li>Designed and developed homepage for Boston Business Hub within Salesforce environment</li>
+								</ul>
+							</div>
+
+							<div>
+								VistaPrint, Web Production Designer 01.2007–01.2008
+							</div>
+							<div>
+								<ul>
+									<li>Designed effective content layout for continuous user experience improvements</li>
+									<li>Executed design concepts into mock-ups for final web page layouts</li>
+									<li>Created effective hero marquees and product offering communications</li>
+									<li>Created mock-ups for new page designs and web application developments</li>
+									<li>Developed and maintained web graphic directories in content management tool</li>
+									<li>Created detailed illustrations and icons used cross culturally for 15 translated web sites</li>
+									<li>Designed graphic updates for monthly merchandising and localization needs</li>
+								</ul>
+							</div>
+
+							<div>
+								Wentworth Institute of Technology, Graphic Designer 07.2000–01.2007
+							</div>
+							<div>
+								<ul>
+									<li>Designed and maintained marketing materials such as: booklets, magazines ads, posters, display panels for on-road recruitment, bookmarks, postcards, brochures, ID card, stickers, and direct mailers, for several institute departments</li>
+									<li>Designed Wentworth’s 100 year centennial logo</li>
+									<li>Developed web graphics for the Admission’s web page, redesigned photo tour, developed landing page for Accepted Student Day, and created header graphics for Wentworth’s web portal</li>
+								</ul>
+							</div>
+
+							</div>
+						</div>
+
+
+
+					<div class="grid-freelance">
+						Freelance
+						<ul>
+							<li>Wentworth Institute of Technology</li>
+							<li>Resolve Technology Advanced Visual Systems</li>
+							<li>Improper Bostonian</li>
+							<li>Brigham and Women’s Hospital</li>
+							<li>Lawyers Weekly</li>
+							<li>Organic Gardening Magazine</li>
+							<li>Weekly Dig</li>
+						</ul>
+
+						Education
+						<ul>
+							<li>Continuing Ed at Mass College of Art Boston, MA 06.2003, 08.2005</li>
+							<li>Bachelor of Arts at Lesley University (formerly Art Institute of Boston) Boston, MA 08.1996–01.1999</li>
+							<li>Rhode Island School of Design Providence, RI 06.1995–01.1996</li>
+							<li>Pennsylvania State University State College, PA  08.1993–01.1995</li>
+						</ul>
+					</div>
+				</div>
+			</section>
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+
+get_footer();
